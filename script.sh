887616bbce3fde0cb1926b28445affea6e394bfc
AngelDLM08 <angeldlm08@gmail.com>
2016-07-05 18:04:39 +0300

    5

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin//bash
+echo "Enter the number"
+     i=64
+read num
+        while [ $i -ne 0 ]
+        do
+         if ($num % 2 = 0)
+        then
+        array[$i]=0
+         else 
+        array[$i]=1
+        fi
+        $i--
+        done
+
+        while ($i -ne 64)
+        do
+        echo array
+        $i++
+        done
+ 
+echo "Enter shift"
+
+read s
+
+echo "Enter side left or right"
+
+read side;
+
+        if ($side = "right")
+        then 
+        
+        fi
